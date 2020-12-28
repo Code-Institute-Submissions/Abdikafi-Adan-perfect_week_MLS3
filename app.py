b8a960f2823ef5c9a966036e9c84336a70e30a89
Abdikafi Adan <Abdikafi.1adan@gmail.com>
2020-12-28 15:04:02 +0000

    reworked  the app   and added new pages

--- app.py
+++ app.py
@@ -1,245 +1,215 @@
 import os
 from flask import (
-    Flask,
-    render_template,
-    redirect,
-    request,
-    url_for,
-    session,
-    flash,
-    Markup
-)
-from bson.objectid import ObjectId
-import bcrypt
+    Flask, flash, render_template,
+    redirect, request, session, url_for)
 from flask_pymongo import PyMongo
-
+from bson.objectid import ObjectId
+from werkzeug.security import generate_password_hash, check_password_hash
 if os.path.exists("env.py"):
     import env
 
+
 app = Flask(__name__)
+
+
+app.config["MONGO_DBNAME"] = os.environ.get("MONGO_DBNAME")
 app.config["MONGO_URI"] = os.environ.get("MONGO_URI")
-app.config['MONGO_DBNAME'] = 'MLS-projetDB'
+app.secret_key = os.environ.get("SECRET_KEY")
+
+
 mongo = PyMongo(app)
-tasks = mongo.db.tasks.find()
-users = mongo.db.users
 
 
-
-# show the index as the starting site and introduces the use to the website
-@app.route('/')
-@app.route('/index')
+@app.route("/")
+@app.route("/index")
 def index():
-    return render_template('index.html')
+    return render_template("index.html")
 
-# the sign in feature and routes
-@app.route('/sign_in', methods=['POST', 'GET'])
-def sign_in():
-    if request.method == "GET":
-        # If a user is already in session, they are flashed a message
-        # confirming this to them, with a prompt to sign out to change account.
-        if 'username' in session:
-            flash(
-                'You\'re already signed in! Sign out ' +
-                'first if you want to change account.')
-            return redirect(url_for('overview'))
 
-        return render_template('signin.html')
+@app.route("/motivation")
+def motivation():
+    return render_template("motivation.html")
 
-    elif request.method == "POST":
-        login_user = users.find_one({'name': request.form['username']})
-        # This checks to see if the hashed password in the form matches the
-        # hashed password in the DB and adds them to the session
-        if login_user:
-            if bcrypt.hashpw(request.form['pass'].encode(
-                    'utf-8'), login_user['password']
-                    ) == login_user['password']:
-                session['username'] = request.form['username']
-                # If correct, the user is sent to their personal overview
-                # page with all task functions available to them. Otherwise
-                # they are flashed a warning message to try again
-                return redirect(url_for('overview'))
+
+@app.route("/get_plans")
+def get_plans():
+    plans = list(mongo.db.plans.find())
+
+    return render_template("plans.html", plans=plans)
+
+
+
+@app.route("/register", methods=["GET", "POST"])
+def register():
+    if request.method == "POST":
+        # check if username already exists in db
+        existing_user = mongo.db.users.find_one(
+            {"username": request.form.get("username").lower()})
+
+        if existing_user:
+            flash("Username already exists")
+            return redirect(url_for("register"))
+
+        register = {
+            "username": request.form.get("username").lower(),
+            "password": generate_password_hash(request.form.get("password"))
+        }
+        mongo.db.users.insert_one(register)
+
+        # put the new user into 'session' cookie
+        session["user"] = request.form.get("username").lower()
+        flash("Registration Successful!")
+        return redirect(url_for("get_plans", username=session["user"]))
+    return render_template("register.html")
+
+
+@app.route("/login", methods=["GET", "POST"])
+def login():
+
+    if request.method == "POST":
+        # check if username exists in db
+        existing_user = mongo.db.users.find_one(
+            {"username": request.form.get("username").lower()})
+
+        if existing_user:
+            # ensure hashed password matches user input
+            if check_password_hash(
+                    existing_user["password"], request.form.get("password")):
+                        session["user"] = request.form.get("username").lower()
+                        flash("Welcome, {}".format(
+                            request.form.get("username")))
+                        return redirect(url_for(
+                            "get_plans", username=session["user"]))
+
             else:
-                flash(
-                    'Oops, it looks like you\'ve entered the wrong' +
-                    ' combination of username and password. Why not' +
-                    ' try again?')
-                return redirect(url_for('sign_in'))
+                # invalid password match
+                flash("Incorrect Username and/or Password")
+                return redirect(url_for("login"))
 
-        elif not login_user:
-            flash(
-                'We don\'t have that username on file! Please check' +
-                ' your spelling and try again.')
-            return redirect(url_for('sign_in'))
+        else:
+            # username doesn't exist
+            flash("Incorrect Username and/or Password")
+            return redirect(url_for("login"))
 
-
-# Routing for new users to sign up, hashing the password for security
-@app.route('/sign_up', methods=['POST', 'GET'])
-def sign_up():
-    if request.method == "GET":
-        # If a user is already signed in, they are flashed a warning
-        # message to sign out to prevent multiple sign-ins
-        if 'username' in session:
-            flash(
-                'You\'re already signed in! Sign out first if ' +
-                'you want to change account.')
-            return redirect(url_for('overview'))
-
-    else:
-        # If there is no existing user in the DB, the entered password
-        # is hashed for security before being sent to store in the DB
-        existing_user = users.find_one({'name': request.form['username']})
-        if existing_user is None:
-            hashpass = bcrypt.hashpw(
-                request.form['pass'].encode('utf-8'), bcrypt.gensalt())
-            users.insert(
-                {'name': request.form['username'], 'password': hashpass})
-            session['username'] = request.form['username']
-            # With the user data stored, they are sent to their new
-            # 'overview' page
-            return redirect(url_for('overview'))
-
-        flash(
-            'Oops, that username already exists! ' +
-            'Please try again with another username.')
-
-    return render_template('signup.html')
+    return render_template("login.html")
 
 
-# Function to logout existing users
-@app.route('/logout')
+# @app.route("/profile/<username>", methods=["GET", "POST"])
+# def profile(username):
+#     # graps the session user's username from db
+#     username = mongo.db.users.find_one(
+#                         {"username": session["user"]})["username"]
+
+#     if session["user"]:
+#         return render_template("profile.html", username=username)
+
+#     return redirect(url_for("login"))
+
+
+@app.route("/logout")
 def logout():
-    username = session['username']
-    if 'username' in session:
-        # This removes session 
-        session.pop('username', None)
-
-        flash(
-            "You've successfully logged out. " +
-            "See you next time, " +
-            username + "!")
-
-        return redirect(url_for('sign_in'))
+    # Loggs out User from sesion cookie
+    flash("You have been  logged out ")
+    session.pop("user")
+    return redirect(url_for("login"))
 
 
-# Routing that shows the user's overview/ dashboard when they have signed in
-# or registered
-@app.route('/overview', methods=['GET', 'POST'])
-def overview():
-    # Redirects a user not in session to the signup page
-    if 'username' not in session:
-        return render_template('signup.html')
+@app.route("/add_plan",  methods=["GET", "POST"])
+def add_plan():
+    # handels the Post resquset from the form in add_plan html
+    if request.method == "POST":
+        is_urgent = "on" if request.form.get("is_urgent") else "off"
+        plan = {
+            "category_name": request.form.get("category_name"),
+            "plan_name": request.form.get("plan_name"),
+            "plan_description": request.form.get("plan_description"),
+            "is_urgent": is_urgent,
+            "due_date": request.form.get("due_date"),
+            "created_by": session["user"]
+        }
+        mongo.db.plans.insert_one(plan)
+        flash("Plan Successfully Added")
+        return redirect(url_for("get_plans"))
 
-    tasks = list(mongo.db.tasks.find({"username": session["username"]}))
-    # Adds completed tasks to completed list
-    completed = []
-
-    if tasks:
-        for task in tasks:
-
-            if task["complete"]:
-                completed.append(task)
-        return render_template(
-            'overview.html',
-            completed_tasks='Complete tasks:',
-            tasks=tasks,
-            completed=completed)
-
-# Routing to direct users to the page where they can begin to add tasks
-@app.route('/new_task')
-def new_task():
-    # Redirects a user not in session to the signup page
-    if 'username' not in session:
-        return render_template('signup.html')
-    return render_template('newtasks.html')
-
-# Function to add user tasks to the DB, collecting username from session
-# to prevent user from providing it again
-@app.route('/add_task', methods=['POST'])
-def add_task():
-    tasks = mongo.db.tasks
-    username = session['username']
-    form_data = {
-        "username": username,
-        "task_title": request.form.get("task_title"),
-        "task_info": request.form.get("task_info"),
-        "task_due": request.form.get("task_due"),
-        "complete": False
-    }
-    tasks.insert_one(form_data)
-
-    return redirect(url_for('overview'))
+    categories = mongo.db.categories.find().sort("category_name", 1)
+    return render_template("add_plan.html", categories=categories)
 
 
-# Routing to direct the user to the edit task page if they are signed in
-@app.route('/edit_task/<task_id>')
-def edit_task(task_id):
-    # Redirects a user not in session to the signup page
-    if 'username' not in session:
-        return render_template('signup.html')
+@app.route("/edit_plan/<plan_id>", methods=["GET", "POST"])
+def edit_plan(plan_id):
+    # handels the Post resquset to upddate the form
+    if request.method == "POST":
+        is_urgent = "on" if request.form.get("is_urgent") else "off"
+        edit_form = {
+            "category_name": request.form.get("category_name"),
+            "plan_name": request.form.get("plan_name"),
+            "plan_description": request.form.get("plan_description"),
+            "is_urgent": is_urgent,
+            "due_date": request.form.get("due_date"),
+            "created_by": session["user"]
+        }
 
-    task = mongo.db.tasks.find_one({'_id': ObjectId(task_id)})
-    return render_template('updatetasks.html', task=task)
+        mongo.db.plans.update({"_id": ObjectId(plan_id)}, edit_form)
+        flash("Plan was Successfully Edited ")
 
+    plan = mongo.db.plans.find_one({"_id": ObjectId(plan_id)})
 
-
-# Function to update a user's task
-@app.route('/update_tasks/<task_id>', methods=['POST', 'GET'])
-def update_tasks(task_id):
-    # Redirects a user not in session to the signup page
-    if 'username' not in session:
-        return render_template('signup.html')
-    # If user is in session continue to update the task
-    username = session['username']
-
-    if request.method == 'POST':
-        updating_task = mongo.db.tasks.find_one_and_update(
-            {"_id": ObjectId(task_id)},
-            {"$set":
-                {"username": username,
-                    "task_title": request.form.get("task_title"),
-                    "task_info": request.form.get("task_info"),
-                    "task_due": request.form.get("task_due"),
-                    "complete": False}}
-        )
-
-        flash('Success, your task has been changed!')
-        return redirect(url_for('overview', task=updating_task))
-
-    else:
-        updating_task = mongo.db.tasks.find_one({"_id": ObjectId(task_id)})
-        return render_template('updatetasks.html', task=updating_task)
+    categories = mongo.db.categories.find().sort("category_name", -1)
+    return render_template("edit_plan.html", plan=plan, categories=categories)
 
 
-# complete plan/task features
-@app.route('/complete_task/<task_id>', methods=['POST', 'GET'])
-def complete_task(task_id):
+# Allows user to delete plan
+@app.route('/delete_task/<plan_id>')
+def delete_plan(plan_id):
 
-    mongo.db.tasks.update(
-        {'_id': ObjectId(task_id)},
-        {"$set": {
-            "complete": True}})
+    mongo.db.plans.remove({'_id': ObjectId(plan_id)})
+    flash("Plan was Successfully Delete")
 
-    return redirect(url_for('overview'))
+    return redirect(url_for('get_plans'))
 
 
-# Allows user to delete plan/task 
-@app.route('/delete_task/<task_id>')
-def delete_task(task_id):
-    mongo.db.tasks.remove({'_id': ObjectId(task_id)})
+@app.route('/get_categories')
+def get_categories():
+    categories = mongo.db.categories.find().sort("category_name", 1)
 
-    return redirect(url_for('overview'))
+    return render_template("categories.html", categories=categories)
 
 
-# Allows a user to delete a completed task from their completed list
-@app.route('/delete_complete_task/<complete_id>')
-def delete_complete_task(complete_id):
-    mongo.db.tasks.remove({'_id': ObjectId(complete_id)})
+@app.route('/add_category', methods=["GET", "POST"])
+def add_category():
+    if request.method == "POST":
+        category = {
+            "category_name": request.form.get("category_name")
+        }
+        mongo.db.categories.insert_one(category)
+        flash("Category was Successfully Add")
+        return redirect(url_for("get_categories"))
 
-    flash("Your completed task has been deleted.")
-    return redirect(url_for('overview'))
+    return render_template("add_category.html")
 
-if __name__ == '__main__':
-    app.secret_key = b'_5#y2L"F4Q8z\n\xec]/'
-    app.run(host=os.environ.get('IP'),
-            port=int(os.environ.get('PORT')),
-            debug=False)
+
+@app.route('/edit_category/<category_id>', methods=["GET", "POST"])
+def edit_category(category_id):
+
+    if request.method == "POST":
+        update = {
+            "category_name": request.form.get("category_name")
+        }
+        mongo.db.categories.update({"_id": ObjectId(category_id)}, update)
+        flash("Category was Successfully Updated")
+        return redirect(url_for("get_categories"))
+    category = mongo.db.categories.find_one({"_id": ObjectId(category_id)})
+    return render_template("edit_category.html", category=category)
+
+
+@app.route('/delete_category/<category_id>')
+def delete_category(category_id):
+    mongo.db.categories.remove({"_id": ObjectId(category_id)})
+    flash("Category was Successfully Deleted")
+    return redirect(url_for("get_categories"))
+
+
+if __name__ == "__main__":
+    app.run(host=os.environ.get("IP"),
+            port=int(os.environ.get("PORT")),
+            debug=True)
